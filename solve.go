147c19fcf170974faf26c8b46d2eb3fcd0a69c9d
Lukas Aron <lukas.aron@gmail.com>
2020-01-16 16:12:46 +1100

    Validation of rows and columns added

--- solve.go
+++ solve.go
@@ -2,36 +2,41 @@ package sudoku
 
 import "errors"
 
+const (
+	rows    = 9
+	columns = 9
+)
+
 var (
 	errOutOfBoardIndex = errors.New("index is out of board")
 	errOutOfBoundValue = errors.New("value is out of bounds, has to be between 1 - 9")
 )
 
 type Board struct {
-	b [9][9]int
+	b [rows][columns]int
 	e error
 }
 
 type Game interface {
-	SetClue(x, y, value int)
-	IsEmpty(x, y int) bool
+	SetClue(row, column, value int)
+	IsEmpty(row, column int) bool
 	IsValid() bool
 }
 
 func NewBoard() Game {
 	return &Board{
-		b: [9][9]int{},
+		b: [rows][columns]int{},
 	}
 }
 
-func (b *Board) SetClue(x, y, value int) {
+func (b *Board) SetClue(row, column, value int) {
 	// do nothing when any error occurred
 	if b.e != nil {
 		return
 	}
 
 	// check for board index value
-	if b.outOfBoard(x, y) {
+	if b.outOfBoard(row, column) {
 		b.e = errOutOfBoardIndex
 		return
 	}
@@ -42,22 +47,22 @@ func (b *Board) SetClue(x, y, value int) {
 		return
 	}
 
-	b.b[x][y] = value
+	b.b[row][column] = value
 }
 
-func (b *Board) IsEmpty(x, y int) bool {
+func (b *Board) IsEmpty(row, column int) bool {
 	// do nothing when any error occurred
 	if b.e != nil {
 		return false
 	}
 
 	// check for board index
-	if b.outOfBoard(x, y) {
+	if b.outOfBoard(row, column) {
 		b.e = errOutOfBoardIndex
 		return false
 	}
 
-	return b.b[x][y] == 0
+	return b.b[row][column] == 0
 }
 
 func (b *Board) IsValid() bool {
@@ -69,6 +74,37 @@ func (b *Board) IsValid() bool {
 	return true
 }
 
-func (b Board) outOfBoard(x, y int) bool {
-	return x < 0 || x > 8 || y < 0 || y > 8
+// ------------------------------------------------- PRIVATE METHODS -------------------------------------------------
+
+func (b Board) outOfBoard(row, column int) bool {
+	return row < 0 || row > (rows-1) || column < 0 || column > (columns-1)
+}
+
+func (b Board) isValidRow(row int) bool {
+	m := [rows + 1]int{}
+	for _, value := range b.b[row] {
+		if m[value] > 0 {
+			return false
+		}
+		m[value] = value
+	}
+
+	return true
+}
+
+func (b Board) isValidColumn(column int) bool {
+	m := [columns + 1]int{}
+	for row := 0; row < rows; row++ {
+		value := b.b[row][column]
+		if m[value] > 0 {
+			return false
+		}
+		m[value] = value
+	}
+
+	return true
+}
+
+func (b Board) isValidBox(box int) bool {
+	return true
 }
